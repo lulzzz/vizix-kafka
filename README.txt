ce006e3f61b09482af2ce5ffea5ebebf992ca709
Renan Huanca <renan.huanca@mojix.com>
2017-05-01 23:10:37 -0400

    added scripts folder

--- README.txt
+++ README.txt
@@ -1,5 +1,10 @@
 Installation guide
 
+Pre-requisites
+
+- docker-compose 1.9.0+
+- valid vizix license
+
 1. cd vizix-kafka
 2. $ docker login
      # username/pass: mojixcloudops/35c-CWN-7mL-akt
@@ -8,6 +13,53 @@ Installation guide
    docker-compose pull
 
 4. run basic containers
+   docker-compose up -d mosquitto
    docker-compose up -d mysql
    docker-compose up -d mongo
-   docker-compose up -d mosquitto
+
+5. start zookeeper and kafka
+   docker-compose up -d zookeeper
+   # wait 10 secs
+   docker-compose up -d kafka
+
+6. init mongo db
+   docker-compose exec mongo sh
+   
+   mongo
+   
+   use admin
+   db.system.users.remove({})
+   db.system.version.remove({})
+   db.dropUser("admin")
+   db.createRole( { role: "executeFunctions", privileges: [ { resource: { anyResource: true }, actions: [ "anyAction" ] } ], roles: [] } )
+   db.createUser({ user: "admin", pwd: "control123!", roles: ["userAdminAnyDatabase", "executeFunctions"] })
+   
+7. create kafka topics
+   docker-compose exec kafka bash
+
+   cd /app/kafka/bin
+
+
+7. run services
+   docker-compose up -d services
+
+8. populate db
+   docker-compose exec services bash
+
+   cd /
+   ./run.sh install retail kafka
+
+9. run ui
+   docker-compose up -d ui      
+
+10. open vizix ui in web brower http://localhost
+
+11. load vizix license
+
+12. update connection settings (mostly hostnames)
+    - kafka 
+    - mongo 
+    - mqtt
+    - services
+    - sql
+
---
+++ scripts/create-topics.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -e
+
+ZOOKEEPER=zookeeper:2181
+KAFKA_HOME=/app/kafka
+
+create_cache_topic ()
+{
+  $KAFKA_HOME/bin/kafka-topics.sh --delete --if-exists --zookeeper $ZOOKEEPER --topic $1
+  $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 1 --partitions $2 --topic $1
+
+  $KAFKA_HOME/bin/kafka-configs.sh --zookeeper $ZOOKEEPER --entity-type topics --entity-name $1 --alter --add-config min.cleanable.dirty.ratio=0.01
+  $KAFKA_HOME/bin/kafka-configs.sh --zookeeper $ZOOKEEPER --entity-type topics --entity-name $1 --alter --add-config cleanup.policy=compact
+  $KAFKA_HOME/bin/kafka-configs.sh --zookeeper $ZOOKEEPER --entity-type topics --entity-name $1 --alter --add-config segment.ms=100
+  $KAFKA_HOME/bin/kafka-configs.sh --zookeeper $ZOOKEEPER --entity-type topics --entity-name $1 --alter --add-config delete.retention.ms=100
+}
+
+create_data_topic ()
+{
+  $KAFKA_HOME/bin/kafka-topics.sh --delete --if-exists --zookeeper $ZOOKEEPER --topic $1
+  $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 1 --partitions $2 --topic $1
+  $KAFKA_HOME/bin/kafka-configs.sh --zookeeper $ZOOKEEPER --entity-type topics --entity-name $1 --alter --add-config retention.ms=1800000
+}
+
+echo "----------------------------"
+echo "- Creating cache topics"
+echo "----------------------------"
+echo
+create_cache_topic ___v1___cache___thingtype 4
+create_cache_topic ___v1___cache___things 4
+create_cache_topic ___v1___cache___zone 4
+create_cache_topic ___v1___cache___zonetype 4
+#create_cache_topic ___v1___cache___localmap 10
+#create_cache_topic ___v1___cache___zoneproperty 10
+create_cache_topic ___v1___cache___group 4
+create_cache_topic ___v1___cache___grouptype 4
+create_cache_topic ___v1___cache___shift 4
+create_cache_topic ___v1___cache___shiftzone 4
+create_cache_topic ___v1___cache___logicalreader 4
+create_cache_topic ___v1___cache___edgebox 4
+create_cache_topic ___v1___cache___edgeboxconfiguration 4
+create_cache_topic ___v1___cache___edgeboxrule 4
+create_cache_topic ___v1___cache___connection 4
+
+echo "----------------------------"
+echo "- Creating data topics"
+echo "----------------------------"
+echo
+create_data_topic ___v1___data1 32 # Edge Boxes(output) and Joiner(input).
+create_data_topic ___v1___data2 32 # Joiner(output) and Kafka Core Bridge(input).
+create_data_topic ___v1___data2___MnS 32 # Joiner(output) and Kafka Core Bridge(input).
+create_data_topic ___v1___data2___BP 32 # Joiner(output) and Kafka Core Bridge(input).
+create_data_topic ___v1___data3 32 # Kafka Core Bridge(output) Mongo Dao(input).
+
+# List all topics.
+$KAFKA_HOME/bin/kafka-topics.sh --list --zookeeper $ZOOKEEPER
